9f52425289d3082cb6a0cae482f25bbb745caaa7
Gaurav Tewari <tewarigaurav10@gmail.com>
2024-08-08 14:54:17 +0530

    updates

--- Document.java
+++ Document.java
@@ -8,10 +8,16 @@ class Document {
         System.out.println("enter 2nd number");
         int n=sc.nextInt();
         System.out.println(add(m,n));
+	System.out.println(sub(m,n));
 
     }
     public static int add(int m,int n){
         int sum=m+n;
         return sum;
     }
+public static int sub(int m,int n){
+        int s=m-n;
+        return s;
+    }
+
 }
