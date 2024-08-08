338d5b52fa14f85018007f5e086a82c258c41e3a
Gopal <jhagopalnayan10@gmail.com>
2024-08-08 14:42:42 +0530

    updated

--- Document.java
+++ Document.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
 class Document {
     public static void main(String args[]) {
-        System.out.println("hello");
+        
+        Scanner sc=new Scanner{System.in};
+        System.out.println("enter 1st number");
+        int m=sc.nextInt();
+        System.out.println("enter 2nd number");
+        int n=sc.nextInt();
+
+    }
+    public static int add(int m,int n){
+        int sum=m+n;
+        return sum;
     }
 }
