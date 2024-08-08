7b929f85a98769a0c233d9f9cbe6221f9622e886
Gopal <jhagopalnayan10@gmail.com>
2024-08-08 15:06:11 +0530

    update made by master

--- Document.java
+++ Document.java
@@ -3,12 +3,31 @@ class Document {
     public static void main(String args[]) {
         
         Scanner sc=new Scanner{System.in};
-        System.out.println("enter 1st number");
-        int m=sc.nextInt();
-        System.out.println("enter 2nd number");
-        int n=sc.nextInt();
-        System.out.println(add(m,n));
-	System.out.println(sub(m,n));
+        
+        System.out.println("choose your option");
+        System.out.println("enter 1 for Addition");
+        System.out.println("enter 2 for Subtraction");
+        int value=sc.nextInt();
+        switch(value){
+            case 1:
+                System.out.println("enter 1st number");
+                int m=sc.nextInt();
+                System.out.println("enter 2nd number");
+                int n=sc.nextInt();
+                System.out.println(add(m,n));
+            break;
+            case 2:
+                System.out.println("enter 1st number");
+                int m=sc.nextInt();
+                System.out.println("enter 2nd number");
+                int n=sc.nextInt();
+                System.out.println(sub(m,n));
+            break;
+            default:
+                System.out.println("invalid input");
+                break;
+
+        }
 
     }
     public static int add(int m,int n){
